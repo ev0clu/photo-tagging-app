33425f79dd5e1b050a7a805dbaa6afa9239e9560
ev0clu <ev0clu@gmail.com>
2023-04-21 09:48:52 +0200

    Tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lilita-one': ['"Lilita One"', 'cursive'],
+        'roboto-sans': ['"Roboto"', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
