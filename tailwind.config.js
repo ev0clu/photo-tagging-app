3125ad20d7be8c8738fa1661fa6697c1bcd0c15c
ev0clu <ev0clu@gmail.com>
2023-04-29 15:53:29 +0200

    Custom style is added

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,17 @@ export default {
         'custom-1':
           'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
         'custom-2': 'rgba(0, 0, 0, 0.35) 0px 5px 15px'
+      },
+
+      keyframes: {
+        feedback: {
+          '0%': { opacity: '0.7' },
+          '50%': { opacity: '1' },
+          '100%': { opacity: '0' }
+        }
+      },
+      animation: {
+        feedback: 'feedback 1s ease-in-out'
       }
     }
   },
