f136a63f301450981caadaacda2eef11057148c3
ev0clu <ev0clu@gmail.com>
2023-05-02 16:44:33 +0200

    Leaderboard component style is updated, firebase data fetching is managed

--- src/pages/leaderboard/Leaderboard.tsx
+++ src/pages/leaderboard/Leaderboard.tsx
@@ -15,29 +15,60 @@ interface Props {
   };
 }
 
+interface ScoreProps {
+  name: string;
+  minute: number;
+  second: number;
+}
+
 const Leaderboard = ({ gameboards }: Props) => {
-  const [selector, setSelector] = useState('');
-  const [scoreData, setScoreData] = useState([]);
+  const [gameSelector, setGameSelector] = useState('1');
+  const [selection, setSelection] = useState('Mountain');
+  const [scoreData, setScoreData] = useState<ScoreProps[]>([]);
 
   const handleSelectorClick = (e: React.MouseEvent<HTMLElement>) => {
     const game = e.currentTarget.dataset.game;
+    if (game !== undefined) {
+      setSelection(game);
+    }
+    let selectedGame = '';
     if (game) {
-      setSelector(game);
+      switch (game) {
+        case 'Mountain':
+          selectedGame = '1';
+          break;
+        case 'Beach':
+          selectedGame = '2';
+          break;
+        case 'Space':
+          selectedGame = '3';
+          break;
+        default:
+          selectedGame = '';
+          break;
+      }
+      setGameSelector(selectedGame);
     }
   };
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getLeaderboard(database);
-      console.log(data);
+      setScoreData(data);
     };
+
     fetchData();
-  }, [selector]);
+  }, [gameSelector]);
 
   const getLeaderboard = async (db: Firestore) => {
-    const scoreCollection = collection(db, `leaderboard`);
+    const scoreCollection = collection(
+      db,
+      `game-${gameSelector}-leaderboard`
+    );
     const scoreSnapshot = await getDocs(scoreCollection);
-    const scoreList = scoreSnapshot.docs.map((doc) => doc.data());
+    const scoreList = scoreSnapshot.docs.map(
+      (doc) => doc.data() as ScoreProps
+    );
 
     return scoreList;
   };
@@ -45,12 +76,13 @@ const Leaderboard = ({ gameboards }: Props) => {
   return (
     <>
       <Header />
-      <main className="flex flex-1 flex-col gap-14 px-64 py-14">
+      <main className="flex flex-1 flex-col items-center justify-start gap-14 px-64 py-14">
         <ul className="flex flex-row justify-between gap-10">
           <LeaderboardSelector
             src={gameboards.game_1_src}
             alt={'game-1-leaderboard'}
             gameName={'Mountain'}
+            selection={selection}
             handleSelectorClick={handleSelectorClick}
           />
 
@@ -58,6 +90,7 @@ const Leaderboard = ({ gameboards }: Props) => {
             src={gameboards.game_2_src}
             alt={'game-2-leaderboard'}
             gameName={'Beach'}
+            selection={selection}
             handleSelectorClick={handleSelectorClick}
           />
 
@@ -65,8 +98,35 @@ const Leaderboard = ({ gameboards }: Props) => {
             src={gameboards.game_3_src}
             alt={'game-3-leaderboard'}
             gameName={'Space'}
+            selection={selection}
             handleSelectorClick={handleSelectorClick}
           />
+        </ul>
+        <ul className="flex flex-col gap-1">
+          {scoreData.map((data, index) => {
+            return (
+              <li
+                key={`score${index}`}
+                data-index={index}
+                className="flex w-96 flex-row justify-between rounded-lg border-4 border-solid border-neutral-200 px-8 py-2 text-lg"
+              >
+                <div className="flex flex-row gap-3">
+                  <div className="font-bold">
+                    {index + 1}
+                    {'.'}
+                  </div>
+                  {data.name}
+                </div>
+                <div>
+                  {data.minute}:
+                  {data.second.toLocaleString('en-US', {
+                    minimumIntegerDigits: 2,
+                    useGrouping: false
+                  })}
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </main>
       <Footer />
--- src/pages/leaderboard/components/LeaderboardSelector.tsx
+++ src/pages/leaderboard/components/LeaderboardSelector.tsx
@@ -3,6 +3,7 @@ interface Props {
   src: string;
   alt: string;
   gameName: string;
+  selection: string;
   handleSelectorClick: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
@@ -10,14 +11,17 @@ const LeaderboardSelector = ({
   src,
   alt,
   gameName,
+  selection,
   handleSelectorClick
 }: Props) => {
   return (
     <>
       <li
         onClick={handleSelectorClick}
-        className="flex cursor-pointer flex-col gap-1 rounded-2xl shadow-custom-2 transition duration-300 ease-in hover:scale-105"
-        data-game={gameName.toLowerCase()}
+        className={`${
+          selection === gameName ? 'scale-105 shadow-amber-600' : ''
+        } flex cursor-pointer flex-col gap-1 rounded-2xl shadow-custom-2 transition duration-300 ease-in hover:scale-105`}
+        data-game={gameName}
       >
         <img
           className="h-56 w-auto rounded-t-2xl"
