38995265b12c71d9b7e1b8e009f372a818af41fb
ev0clu <ev0clu@gmail.com>
2023-05-04 10:25:11 +0200

    Loading feature is separated to a function component

--- src/pages/game/components/GameoverModal.tsx
+++ src/pages/game/components/GameoverModal.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import Button from '../../../elements/Button';
 import Input from '../../../elements/Input';
+import Loading from '../../../components/Loading';
 
 interface Props {
   minute: number;
@@ -29,36 +30,11 @@ const GameoverModal = ({ minute, second, submitScore }: Props) => {
     }
   };
 
-  const loading = () => {
-    return (
-      <svg
-        className="-ml-1 mr-3 h-14 w-14 animate-spin text-amber-600"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          className="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          strokeWidth="4"
-        ></circle>
-        <path
-          className="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    );
-  };
-
   return (
     <div className="fixed left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-10 rounded-2xl  bg-white px-10 py-10 shadow-custom-1">
       <div className="flex h-28 w-96 flex-col items-center justify-center gap-4">
         {submit ? (
-          loading()
+          <Loading />
         ) : (
           <>
             <div className="text-2xl">
